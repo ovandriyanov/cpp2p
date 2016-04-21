9270e72fe57ed1606d65feb5279ab8d36258551c
Oleg Andriyanov <o.andriyanov@gmail.com>
2016-04-21 22:26:27 +0300

    Check if compiles

---
+++ cpp2p/udp4/impl/hole_puncher.cpp
@@ -0,0 +1,1 @@
+#include "cpp2p/udp4/impl/hole_puncher.h"
---
+++ cpp2p/udp4/impl/hole_puncher.h
@@ -0,0 +1,6 @@
+#ifndef CPP2P_UDP4_IMPL_HOLE_PUNCHER_H
+#define CPP2P_UDP4_IMPL_HOLE_PUNCHER_H
+
+#include "cpp2p/udp4/hole_puncher.h"
+
+#endif // CPP2P_UDP4_IMPL_HOLE_PUNCHER_H
