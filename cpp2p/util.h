b2648ae168dd45ba3e68358444cf0aac09d3ac9b
Oleg Andriyanov <o.andriyanov@gmail.com>
2016-06-05 02:46:12 +0300

    cpp2p::concat() utility

--- cpp2p/util.h
+++ cpp2p/util.h
@@ -9,11 +9,20 @@
 #ifndef CPP2P_UTIL_H
 #define CPP2P_UTIL_H
 
+#include <sstream>
 #include <utility>
 
 namespace cpp2p {
 
 template <typename T> using two_t = std::pair<T, T>;
+
+template <typename... T>
+std::string concat(const T&... args)
+{
+    std::ostringstream os;
+    (int[]){ (os << args, 0)... };
+    return os.str();
+}
 
 } // namespace cpp2p
 
